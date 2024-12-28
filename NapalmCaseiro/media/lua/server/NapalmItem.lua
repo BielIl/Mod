f386edb3094fe694ccf944f20f614b959f1bdb30
BielIl <gabrielluisnolascooficial@gmail.com>
2024-12-27 23:38:49 -0300

    0.2

--- NapalmCaseiro/media/lua/server/NapalmItem.lua
+++ NapalmCaseiro/media/lua/server/NapalmItem.lua
@@ -1,42 +1,41 @@
--- Certifique-se de que InventoryItemFactory, Events e getScriptManager estão disponíveis
-local InventoryItemFactory = require "InventoryItemFactory"
-local Events = require "Events"
-local getScriptManager = require "getScriptManager"
+-- Obtém as bibliotecas necessárias
+local InventoryItemFactory = require "InventoryItemFactory" -- Para criar itens no jogo
+local Events = require "Events"                             -- Para usar eventos do jogo
+local getScriptManager = require "getScriptManager"         -- Para registrar o item no jogo
 
--- Definindo o item Napalm Caseiro
+-- Define a tabela NapalmCaseiro para armazenar as propriedades do item
 NapalmCaseiro = {}
 
-NapalmCaseiro.Type = "InventoryItem"          -- Tipo de item (Item de inventário)
-NapalmCaseiro.DisplayName = "Napalm Caseiro"  -- Nome do item
-NapalmCaseiro.Icon = "Molotov"                -- Ícone do item (você pode personalizar o ícone)
-NapalmCaseiro.Weight = 1.5                    -- Peso do item
-NapalmCaseiro.ImpactSound = "BreakObject"     -- Som do impacto
-NapalmCaseiro.CustomContextMenu = true        -- Permite adicionar um menu de contexto customizado
-NapalmCaseiro.IsExplosive = true              -- Define que o item é explosivo
-NapalmCaseiro.ExplosionTimer = 1              -- Tempo de explosão em segundos
+-- Define as propriedades do item Napalm Caseiro
+NapalmCaseiro.Type = "InventoryItem"         -- Tipo de item
+NapalmCaseiro.DisplayName = "Napalm Caseiro" -- Nome que será exibido no jogo
+NapalmCaseiro.Icon = "Molotov"               -- Ícone do item (pode ser alterado)
+NapalmCaseiro.Weight = 1.5                   -- Peso do item
+NapalmCaseiro.ImpactSound = "BreakObject"    -- Som que será reproduzido ao usar o item
+NapalmCaseiro.CustomContextMenu = true       -- Permite adicionar um menu de contexto customizado
+NapalmCaseiro.IsExplosive = true             -- Define que o item é explosivo
+NapalmCaseiro.ExplosionTimer = 1             -- Tempo em segundos até a explosão após o uso
 
--- Função que é chamada quando o item é usado
+-- Função que define o que acontece quando o item é usado
 function NapalmCaseiro.UseNapalmCaseiro(player)
-    -- Função que chama a explosão do napalm (você pode integrar com a função OnExplode do seu código anterior)
-    Napalm.OnExplode(player:getSquare())
+    -- Chama a função ExplodeNapalm do NapalmEffect para causar a explosão
+    Napalm.ExplodeNapalm(player:getSquare())
 end
 
--- Função para criar o item no jogo
+-- Função que cria o item Napalm Caseiro no jogo
 function NapalmCaseiro.CreateNapalmCaseiroItem()
-    -- Registrando o item no sistema de itens do Project Zomboid
-    local item = InventoryItemFactory.CreateItem("NapalmCaseiro")  -- Usa o ID do item
+    -- Cria o item usando o InventoryItemFactory
+    local item = InventoryItemFactory.CreateItem("NapalmCaseiro")
+    -- Define as propriedades do item
     item:setWeight(NapalmCaseiro.Weight)
     item:setIcon(NapalmCaseiro.Icon)
-    
-    -- Se precisar configurar algum som ou outras propriedades
     item:setImpactSound(NapalmCaseiro.ImpactSound)
-    
-    -- Retorna o item
+    -- Retorna o item criado
     return item
 end
 
--- Registrando o item para que ele seja reconhecido no jogo
+-- Adiciona um evento que ocorre quando o jogo inicia
 Events.OnGameBoot.Add(function()
-    -- Adiciona o item ao sistema quando o jogo iniciar
+    -- Registra o item Napalm Caseiro no jogo usando o getScriptManager
     getScriptManager():AddItem("NapalmCaseiro", NapalmCaseiro)
 end)
--- NapalmCaseiro/media/lua/server/NapalmRecipe.lua
+++ NapalmCaseiro/media/lua/server/NapalmRecipe.lua
@@ -1,30 +1,34 @@
--- Certifique-se de que Events e RecipeManager estão disponíveis
-local Events = require "Events"
-local RecipeManager = require "RecipeManager"
+-- Obtém as bibliotecas necessárias
+local Events = require "Events"               -- Para usar eventos do jogo
+local RecipeManager = require "RecipeManager" -- Para adicionar receitas de crafting
 
+-- Define a tabela NapalmRecipe para armazenar a receita do Napalm Caseiro
 local NapalmRecipe = {
+    -- Define a receita
     Recipe = {
-        Name = "Fabricar Napalm Caseiro",
-        Description = "Misture ingredientes simples para criar um explosivo perigoso.",
-        RequiredItems = {
-            { "Base.Gasoline", 1 },        -- Gasolina
-            { "Base.OrangeSoda", 1 },     -- Suco de laranja
-            { "Base.Bleach", 1 },         -- Detergente (alvejante)
-            { "Base.WaterBottleEmpty", 1 } -- Garrafa plástica vazia
+        Name = "Fabricar Napalm Caseiro",                                               -- Nome da receita
+        Description = "Misture ingredientes simples para criar um explosivo perigoso.", -- Descrição da receita
+        RequiredItems = {                                                               -- Itens necessários para fabricar o Napalm Caseiro
+            { "Base.Gasoline",         1 },                                             -- Gasolina
+            { "Base.OrangeSoda",       1 },                                             -- Refrigerante de laranja
+            { "Base.Bleach",           1 },                                             -- Água sanitária
+            { "Base.WaterBottleEmpty", 1 }                                              -- Garrafa de água vazia
         },
-        ResultItem = "NapalmCaseiro.NapalmCaseiro", -- O item resultante
-        SkillRequired = { "Cooking", 3 },
-        TimeToCraft = 200,  -- Tempo de crafting em segundos (ajustável conforme necessidade)
-        OnCreate = function(player)
-            -- Ação extra que pode ser realizada quando o item for criado
+        ResultItem = "NapalmCaseiro.NapalmCaseiro",                                     -- Item resultante da receita
+        SkillRequired = { "Cooking", 3 },                                               -- Habilidade e nível necessários para fabricar
+        TimeToCraft = 200,                                                              -- Tempo em segundos para fabricar o item
+        OnCreate = function(player)                                                     -- Função que é executada quando o item é fabricado
+            -- Exibe uma mensagem para o jogador
             player:Say("Napalm Caseiro fabricado com sucesso!")
         end
     }
 }
 
--- Registrando a receita no sistema de crafting
+-- Adiciona um evento que ocorre quando o jogo inicia
 Events.OnGameBoot.Add(function()
+    -- Adiciona a receita do Napalm Caseiro ao jogo
     RecipeManager.addRecipe(NapalmRecipe.Recipe)
 end)
 
+-- Retorna a tabela NapalmRecipe
 return NapalmRecipe
